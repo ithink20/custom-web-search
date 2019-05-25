55b52ab029290948b8a0199325a84ab79d8b02a7
ithink20 <suicidesquade21@gmail.com>
2019-05-25 11:53:03 -0700

    implemented _ls

--- server.py
+++ server.py
@@ -13,31 +13,38 @@ def run(host_name, port_number, handler_class):
     httpd.serve_forever()
 
 def load_config():
-    print("here")
-    with open("url_config.yaml", "r") as data:
-        try:
-            return yaml.safe_load(data)
-        except yaml.YAMLError as err:
-            print(err)
+    with open("url_config.yaml", "r") as file_object:
+        return yaml.safe_load(file_object)
 
 class handle(BaseHTTPRequestHandler):
     def do_GET(self):
         # print (self.requestline, self.path)
-        query = urllib.parse.unquote(self.path).split(" ", 1)
-        type = query[0]
-        flag = False
-        for item in dataMap:
-            if (item == type[1:]):
-                query_string = urllib.parse.quote_plus(query[1])
-                search_url = dataMap[item] + query_string
-                self.send_response(HTTPStatus.FOUND)
-                self.send_header("Location", search_url)
-                self.end_headers()
-                flag = True
-                break
-        if not flag:
-            self.send_response(HTTPStatus.NOT_FOUND)
-            self.end_headers()
+        query = urllib.parse.unquote(self.path[1:]) # remove leading '/' in path
+        query_components = query.split(" ", 1)
+        if query_components[0] in dataMap:
+            type = query_components[0]
+            query_args = query_components[1] if len(query_components) > 1 else ""
+        elif query_components[0] == '_ls':
+            self.process_ls()
+            return
+        else:
+            type = 'g'
+            query_args = query
+
+        query_args = urllib.parse.quote_plus(query_args)
+        redirect_url = dataMap[type] + query_args
+        self.send_response(HTTPStatus.FOUND)
+        self.send_header("Location", redirect_url)
+        self.end_headers()
+    
+    def process_ls(self):
+        self.send_response(HTTPStatus.FOUND)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open("url_config.yaml", "r") as file_object:
+            content = "<html><body><pre>{}</pre></body></html>".format(file_object.read())
+        response = bytes(content, 'utf-8')
+        self.wfile.write(response)
 
 if __name__ == "__main__":
     dataMap = load_config()
