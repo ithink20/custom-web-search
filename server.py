32c151f027a33769c7c35da589709a57c823600f
ithink20 <suicidesquade21@gmail.com>
2019-05-25 03:52:19 -0700

    added config.yaml

--- server.py
+++ server.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python3
 
-import http.server, urllib
+import http.server, urllib, yaml
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
 from urllib.parse import urlparse
 
+dataMap = None
 
 def run(host_name, port_number, handler_class):
     server_address = (host_name, port_number)
     httpd = HTTPServer(server_address, handler_class)
     httpd.serve_forever()
 
+def load_config():
+    print("here")
+    with open("url_config.yaml", "r") as data:
+        try:
+            return yaml.safe_load(data)
+        except yaml.YAMLError as err:
+            print(err)
+
 class handle(BaseHTTPRequestHandler):
     def do_GET(self):
-        print (self.requestline, self.path)
+        # print (self.requestline, self.path)
         query = urllib.parse.unquote(self.path).split(" ", 1)
-        print (query)
         type = query[0]
-        if (type == '/g'):
-            query_string = urllib.parse.quote_plus(query[1])
-            search_url = "https://www.google.com/search?q=" + query_string
-            response = "<html><body><body>{}</html>".format(search_url).encode('utf-8')
-            self.send_response(HTTPStatus.FOUND)
-            self.send_header("Location", search_url)
-            self.end_headers()
-        else:
+        flag = False
+        for item in dataMap:
+            if (item == type[1:]):
+                query_string = urllib.parse.quote_plus(query[1])
+                search_url = dataMap[item] + query_string
+                self.send_response(HTTPStatus.FOUND)
+                self.send_header("Location", search_url)
+                self.end_headers()
+                flag = True
+                break
+        if not flag:
             self.send_response(HTTPStatus.NOT_FOUND)
             self.end_headers()
 
 if __name__ == "__main__":
+    dataMap = load_config()
     run('localhost', 12345, handle)
